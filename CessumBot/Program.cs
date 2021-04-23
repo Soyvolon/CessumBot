e792de89a1ffdd13d97ed0f1f45020e0ed3c8866
Andrew Bounds <andybounds@gmail.com>
2021-04-23 11:58:23 -0400

    Add project files.

---
+++ CessumBot/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CessumBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => Main().GetAwaiter().GetResult();
+
+        public static async Task Main()
+        {
+
+        }
+    }
+}
